b62f9217a0874a760ab9607437f96f7d7aaf31bf
jaejunha <dreamline91@naver.com>
2019-07-07 21:45:13 +0900

    Modularization(ing...)

---
+++ sub/memo.py
@@ -0,0 +1,33 @@
+﻿from tkinter import *
+import shutil
+import os
+
+"""
+Save free memo
+"""
+def saveFree(text):
+
+	os.chdir("C:\\Desktop")
+	
+	shutil.copyfile("memo.txt", "memo.bak1")
+	
+	file = open("memo.txt", "w")
+	file.write(text)
+	file.close()
+
+	shutil.copyfile("memo.txt", "memo.bak2")
+
+"""
+Load free memo
+"""
+def loadFree(free):
+
+	try:
+		os.chdir("C:\\Desktop")
+		file = open("memo.txt", "r")
+
+		for line in file.readlines():
+			free.insert(END, line)
+		file.close()
+	except NameError:
+		return
---
+++ sub/system.py
@@ -0,0 +1,22 @@
+﻿from tkinter import *
+
+LOW_ALPHA = 0.2
+HIGH_ALPHA = 1
+
+# alpha (used at hide mode)
+alpha = HIGH_ALPHA
+
+"""
+Hide(semi) mode
+"""
+def hideWindow(window):
+	global alpha
+
+	if alpha == HIGH_ALPHA:
+		alpha = LOW_ALPHA
+		window.wm_attributes("-topmost", False)
+	else:
+		alpha = HIGH_ALPHA
+		window.wm_attributes("-topmost", True)
+
+	window.attributes("-alpha", alpha)
